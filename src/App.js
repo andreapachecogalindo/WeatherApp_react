13f7622340e0d15cbacb330067e55aa7eeeaa530
Andrea Pacheco <67153792+andreapachecogalindo@users.noreply.github.com>
2020-12-28 19:35:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header
+        defaultCity="Barcelona"
+        unitC={{
+          color: `#fcfeff`,
+          backgroundColor: `#465866`,
+          borderRadius: `50%`,
+          cursor: `default`
+        }}
+      />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      . Code on{" "}
+      <a href="" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
